23d6ab0a771e95723501e77ca2489055e3d75c7d
Márcio Andrade <marcioada@gmail.com>
2022-02-05 21:46:35 -0300

    Atualização do controller Estado

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,8 +3,10 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.model.Cozinha;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,11 +19,13 @@ public class CozinhaController {
 
     @GetMapping
     public List<Cozinha> todas(){
+
         return cozinhaRepository.todas();
     }
 
     @GetMapping("/{cozinhaId}")
     public Cozinha porId(@PathVariable Long cozinhaId){
+
         return cozinhaRepository.porId(cozinhaId);
     }
 
--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -6,6 +6,7 @@ import com.algaworks.algafood.model.Cozinha;
 import com.algaworks.algafood.model.Estado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class EstadoController {
     public List<Estado> todas(){
 
         return estadoRepository.todas();
+    }
+
+    @GetMapping("/{estadoId}")
+    public Estado porId(@PathVariable Long estadoId){
+        return estadoRepository.porId(estadoId);
     }
 
 }
