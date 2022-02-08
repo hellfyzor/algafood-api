73c4518b0773b09062394cf2fd80e1a9b83b89da
Márcio Andrade <marcioada@gmail.com>
2022-02-07 21:20:14 -0300

    Atualização do controller cozinha adicionando o endpoint adicionar

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,11 +3,9 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.model.Cozinha;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +33,12 @@ public class CozinhaController {
 
         return ResponseEntity.notFound().build();
 
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar (@RequestBody Cozinha cozinha){
+        return cozinhaRepository.adicionar(cozinha);
     }
 
 }
