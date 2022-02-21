6873f10cf70176bbcd3cb638b629c6c1a8b99d65
Márcio Andrade <marcioada@gmail.com>
2022-02-21 17:25:03 -0300

    Ajustando a consulta dinamica parte 2

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +27,14 @@ public class RestauranteRepositoryImpl implements com.algaworks.algafood.domain.
         CriteriaBuilder builder = manager.getCriteriaBuilder();
 
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-        criteria.from(Restaurante.class);
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
